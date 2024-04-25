7c935579829aaa4af13f513711712f7a4f138460
nikoDev <nikolas090189@gmail.com>
2024-04-25 17:04:33 -0300

    Cors added

--- src/app.ts
+++ src/app.ts
@@ -3,9 +3,11 @@ import driversRoutes from "./routes/drivers.routes";
 import vehiclesRoutes from "./routes/vehicles.routes";
 import tripsRoutes from "./routes/trips.routes";
 import { Paths } from "./routes/paths";
+import cors from "cors";
 
 export const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
