57f811e6efabbc9268d52d68f06db09cec1109c0
nikoDev <nikolas090189@gmail.com>
2024-04-25 17:33:49 -0300

    Setup base de la suite de test

--- src/infraestructure/adapters/database.adapter.ts
+++ src/infraestructure/adapters/database.adapter.ts
@@ -5,6 +5,7 @@ let sequelize: Sequelize | null = null;
 export function getDatabaseInstance(): Sequelize {
   if (!sequelize) {
     sequelize = new Sequelize("virtualizadb", "root", "myrootpassword", {
+      logging: false,
       host: "localhost",
       dialect:
         "mysql" /* one of 'mysql' | 'postgres' | 'sqlite' | 'mariadb' | 'mssql' | 'db2' | 'snowflake' | 'oracle' */,
